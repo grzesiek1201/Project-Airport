5a8e5bf3bbf396c3ed4d3a25dd05782a404e1aca
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-06-12 18:15:16 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -2,17 +2,18 @@ import socket
 
 
 class Client:
-    def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect(("127.0.0.1", 65432))
+    def __init__(self, host='127.0.0.1', port=65432):
+        self.host = host
+        self.port = port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send_response(self, message):
-        try:
-            self.s.sendall(message.encode("utf-8"))
-            data = self.s.recv(1024)
-        except socket.error as e:
-            print("Socket error:", e)
+    def connect(self):
+        self.client_socket.connect((self.host, self.port))
 
+    def send(self, message):
+        self.client_socket.sendall(message.encode())
+        response = self.client_socket.recv(1024)
+        print(f'Received from server: {response.decode()}')
 
-if __name__ == "__main__":
-    client = Client()
+    def disconnect(self):
+        self.client_socket.close()
