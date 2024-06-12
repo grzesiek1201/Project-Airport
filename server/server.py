5be3d692f138f864f8b51f72f25daef6c9ed8308
Grzegorz <138705337+grzesiek1201@users.noreply.github.com>
2024-06-12 18:15:46 +0200

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,68 +1,22 @@
 import socket
+from .handler import ClientHandler
 
 
 class Server:
-    def __init__(self, host, port, version):
+    def __init__(self, host='127.0.0.1', port=65432):
         self.host = host
         self.port = port
-        self.version = version
-
-    class CommandError(Exception):
-        pass
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
 
     def start(self):
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind((self.host, self.port))
-                s.listen()
-                print(f"Server listening on {self.host}: {self.port}")
-                while True:
-                    try:
-                        conn, addr = s.accept()
-                        with conn:
-                            print(f"Connected to {addr}")
-                            while True:
-                                data = conn.recv(1024).decode("utf-8")
-                                if not data:
-                                    break
-                                response = self.handle_command(data, conn, addr)
-                                if response is None:
-                                    print("Disconnected")
-                                    break
-                                conn.sendall(response.encode("utf-8"))
-                    except KeyboardInterrupt:
-                        print("KeyboardInterrupt: Stopping server...")
-                        break
-                    except Exception as e:
-                        print(f"Error accepting connection: {e}")
-        except OSError as e:
-            print(f"Error binding to address {self.host}: {self.port}: {e}")
+        print(f'Server listening on {self.host}:{self.port}')
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            print(f'Connection from {client_address}')
+            handler = ClientHandler(client_socket)
+            handler.handle()
 
-    def handle_command(self, command, conn, addr):
-        try:
-            command_parts = command.split()
-            if not command_parts:
-                raise self.CommandError("Empty command")
-
-            command_name = command_parts[0].lower()
-            command_args = command_parts[1:]
-
-            command_handlers = {
-            }
-
-            handler = command_handlers.get(command_name)
-            if not handler:
-                raise self.CommandError("Unknown command")
-
-            self.response = handler(*command_args)
-            return self.response
-        except Exception as e:
-            return str(e)
-
-
-if __name__ == "__main__":
-    server_host = "127.0.0.1"
-    server_port = 65432
-    server_version = "version: 0.1"
-    server = Server(server_host, server_port, server_version)
-    server.start()
+    def stop(self):
+        self.server_socket.close()
